b3e3f2c4cc0c93c16f730cdaa197854ae2801baf
DANIELA LETUNICA <dletunic@10-150-45-14.dhcp-m.deakin.edu.au>
2017-04-20 13:05:51 +1000

    test

---
+++ Poject2/Poject2Tests/Poject2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Poject2Tests.swift
+//  Poject2Tests
+//
+//  Created by DANIELA LETUNICA on 20/04/2017.
+//  Copyright © 2017 DANIELA LETUNICA. All rights reserved.
+//
+
+import XCTest
+@testable import Poject2
+
+class Poject2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

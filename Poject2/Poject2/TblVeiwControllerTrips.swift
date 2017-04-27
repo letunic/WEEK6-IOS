8ce18c6c138d5b7f06bcb1e38e029b97b1d034ac
DANIELA LETUNICA <dletunic@10-150-45-14.dhcp-m.deakin.edu.au>
2017-04-27 13:47:25 +1000

    map

--- Poject2/Poject2/TblVeiwControllerTrips.swift
+++ Poject2/Poject2/TblVeiwControllerTrips.swift
@@ -11,22 +11,7 @@ import UIKit
 
 class TblViewControllerTrips: UITableViewController{
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        utilities.loadTrips()
-        
-        self.tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
-        
-    }
+
     
-    override func numberOfSections(in tableView: UITableView) -> int {
-        return 1
-    }
-    override func tableView(_ tableView: UITableView,
-                            numberOfRowsInSection section: Int) {
-        return utilities.trips.count;
-    }
-    
-    override
+
 }
